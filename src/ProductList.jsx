ab6f8f0f54502f6bdd58709aa68372ef53496020
sebastiantuberquia <stuberquia3@gmail.com>
2024-12-24 16:06:27 -0600

    First commit. Some arrangements

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 function ProductList() {
